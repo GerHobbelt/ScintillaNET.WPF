6a825ca59b25bbed5500b8eecbf081a66a02a0a7
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-03-01 19:26:52 +0000

    Add some line automations

--- ScintillaNET.WPF/Automation/TextRangeProvider.cs
+++ ScintillaNET.WPF/Automation/TextRangeProvider.cs
@@ -8,20 +8,20 @@ namespace ScintillaNET.WPF.Automation
     {
         private readonly ScintillaWPF editor;
 
-        public int Start { get; private set; }
+        public int Position { get; private set; }
 
-        public int End { get; private set; }
+        public int EndPosition { get; private set; }
 
-        public TextRangeProvider(ScintillaWPF owner, int start, int end)
+        public TextRangeProvider(ScintillaWPF owner, int position, int endPosition)
         {
             this.editor = owner;
-            this.Start = start;
-            this.End = end;
+            this.Position = position;
+            this.EndPosition = endPosition;
         }
 
         public ITextRangeProvider Clone()
         {
-            return new TextRangeProvider(this.editor, this.Start, this.End);
+            return new TextRangeProvider(this.editor, this.Position, this.EndPosition);
         }
 
         public bool Compare(ITextRangeProvider range)
@@ -31,7 +31,7 @@ namespace ScintillaNET.WPF.Automation
                 return false;
             }
 
-            return this.Start == provider.Start && this.End == provider.End;
+            return this.Position == provider.Position && this.EndPosition == provider.EndPosition;
         }
 
         public int CompareEndpoints(TextPatternRangeEndpoint endpoint, ITextRangeProvider targetRange, TextPatternRangeEndpoint targetEndpoint)
@@ -41,118 +41,160 @@ namespace ScintillaNET.WPF.Automation
                 return 0;
             }
 
-            var position = endpoint == TextPatternRangeEndpoint.Start ? this.Start : this.End;
-            var targetPosition = targetEndpoint == TextPatternRangeEndpoint.Start ? provider.Start : provider.End;
+            var position = endpoint == TextPatternRangeEndpoint.Start ? this.Position : this.EndPosition;
+            var targetPosition = targetEndpoint == TextPatternRangeEndpoint.Start ? provider.Position : provider.EndPosition;
             return position - targetPosition;
         }
 
         public void ExpandToEnclosingUnit(TextUnit unit)
         {
-            throw new System.NotImplementedException();
+            switch (unit)
+            {
+                case TextUnit.Character:
+                    this.EndPosition = this.Position + 1;
+                    break;
+                case TextUnit.Line:
+                {
+                    var line = this.editor.LineFromPosition(this.Position);
+                    this.Position = this.editor.Lines[line].Position;
+                    this.EndPosition = this.editor.Lines[line].EndPosition;
+                    break;
+                }
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         public ITextRangeProvider FindAttribute(int attribute, object value, bool backward)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public ITextRangeProvider FindText(string text, bool backward, bool ignoreCase)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public object GetAttributeValue(int attribute)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public double[] GetBoundingRectangles()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public IRawElementProviderSimple GetEnclosingElement()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public string GetText(int maxLength)
         {
-            var length = this.End - this.Start;
+            var length = this.EndPosition - this.Position;
             if (maxLength >= 0)
             {
                 length = Math.Min(maxLength, length);
             }
 
-            return this.editor.GetTextRange(this.Start, length);
+            return this.editor.GetTextRange(this.Position, length);
         }
 
         public int Move(TextUnit unit, int count)
         {
-            if (unit == TextUnit.Character)
+            switch (unit)
             {
-                var previousStart = this.Start;
-                var degenerate = this.Start == this.End;
-                this.Start = Math.Max(0, Math.Min(this.editor.TextLength - 1, this.Start + count));
-                this.End = this.Start + (degenerate ? 0 : 1);
-                return this.Start - previousStart;
+                case TextUnit.Character:
+                {
+                    var previousStart = this.Position;
+                    this.Position = Math.Max(0, Math.Min(this.editor.TextLength - 1, this.Position + count));
+                    this.EndPosition = this.Position + 1;
+                    return this.Position - previousStart;
+                }
+                case TextUnit.Line:
+                {
+                    var previous = this.editor.LineFromPosition(this.Position);
+                    var newIndex = Math.Max(0, Math.Min(this.editor.Lines.Count - 1, previous + count));
+                    var newLine = this.editor.Lines[newIndex];
+                    this.Position = newLine.Position;
+                    this.EndPosition = newLine.EndPosition;
+                    return newIndex - previous;
+                }
+                default:
+                    throw new NotImplementedException();
             }
-
-            // TODO
-            throw new System.NotImplementedException();
         }
 
         public int MoveEndpointByUnit(TextPatternRangeEndpoint endpoint, TextUnit unit, int count)
         {
-            if (unit == TextUnit.Character)
+            switch (unit)
             {
-                if (endpoint == TextPatternRangeEndpoint.Start)
+                case TextUnit.Character:
                 {
-                    var previous = this.Start;
-                    this.Start = Math.Max(0, Math.Min(this.End, this.Start + count));
-                    return this.Start - previous;
+                    if (endpoint == TextPatternRangeEndpoint.Start)
+                    {
+                        var previous = this.Position;
+                        this.Position = Math.Max(0, Math.Min(this.EndPosition, this.Position + count));
+                        return this.Position - previous;
+                    }
+                    else
+                    {
+                        var previous = this.EndPosition;
+                        this.EndPosition = Math.Max(this.Position, Math.Min(this.editor.TextLength, this.EndPosition + count));
+                        return this.EndPosition - previous;
+                    }
                 }
-                else
+                case TextUnit.Line:
                 {
-                    var previous = this.End;
-                    this.End = Math.Max(this.Start, Math.Min(this.editor.TextLength - 1, this.End + count));
-                    return this.End - previous;
-                }
-            }
+                    var previous = this.editor.LineFromPosition(endpoint == TextPatternRangeEndpoint.Start ? this.Position : this.EndPosition);
+                    var newIndex = Math.Max(0, Math.Min(this.editor.Lines.Count - 1, previous + count));
+                    var newLine = this.editor.Lines[newIndex];
+                    if (endpoint == TextPatternRangeEndpoint.Start)
+                    {
+                        this.Position = newLine.Position;
+                    }
+                    else
+                    {
+                        this.EndPosition = newLine.EndPosition;
+                    }
 
-            // TODO
-            throw new System.NotImplementedException();
+                    return newIndex - previous;
+                }
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         public void MoveEndpointByRange(TextPatternRangeEndpoint endpoint, ITextRangeProvider targetRange, TextPatternRangeEndpoint targetEndpoint)
         {
             // TODO
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void Select()
         {
-            this.editor.SetSelection(this.End, this.Start);
+            this.editor.SetSelection(this.EndPosition, this.Position);
         }
 
         public void AddToSelection()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void RemoveFromSelection()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void ScrollIntoView(bool alignToTop)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public IRawElementProviderSimple[] GetChildren()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
